0aa18d4ef4a1e37d039b14c32d45f87b3b500e47
Nikolay Tsvetkov <nic.tsvetcov@gmail.com>
2020-11-25 11:49:14 +0300

    Add tests to the SeleniumWebDrivertest

--- src/main/java/SeleniumWebDriver.java
+++ src/main/java/SeleniumWebDriver.java
@@ -1,8 +1,37 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 public class SeleniumWebDriver {
-    WebDriver driver = new ChromeDriver();
 
+    public static WebDriver driver;
+    public static WebElement searchInput;
+    public static WebElement searchButton;
+    public static WebElement searchSneakers;
+    public static WebElement searchSizeButton;
 
+    public static void initialization() {
+        driver = new ChromeDriver();
+
+        driver.get("https://nbalance.by/");
+        driver.manage().window().maximize();
+
+        searchInput = waitForElementLocatedBy(driver ,By.xpath("//input[@class = 'yith-s']"));
+        searchInput.sendKeys("New Balance 574 Dark");
+        searchButton = waitForElementLocatedBy(driver, By.xpath("//input[@id='yith-searchsubmit']"));
+        searchButton.click();
+        searchSneakers = waitForElementLocatedBy(driver, By.xpath("//a[contains(@class, 'woocommerce-LoopProduct-link') and h2[text() = 'New Balance 574 Dark']]"));
+        searchSneakers.click();
+        searchSizeButton = waitForElementLocatedBy(driver, By.xpath("//span[contains(@class, 'swatch-8-5')]"));
+        searchSizeButton.click();
+    }
+
+    private static WebElement waitForElementLocatedBy(org.openqa.selenium.WebDriver driver, By by) {
+        return new WebDriverWait(driver, 10)
+                .until(ExpectedConditions.visibilityOfElementLocated(by));
+    }
 }
--- src/test/java/SeleniumWebDriverTest.java
+++ src/test/java/SeleniumWebDriverTest.java
@@ -1,3 +1,33 @@
-public class SeleniumWebDriverTest {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
+public class SeleniumWebDriverTest  extends SeleniumWebDriver {
+
+    @BeforeTest
+    public void setup() {
+        initialization();
+    }
+
+    @Test
+    public void shouldBeSelectSizeTest() {
+        WebElement actualSize = driver.findElement(By.xpath("//span[contains(@class, 'swatch-8-5')]"));
+        WebElement selectedSize = driver.findElement(By.xpath("//span[contains(@class, 'selected')]"));
+        Assert.assertEquals(actualSize, selectedSize);
+    }
+
+    @Test
+    public void verifySneakersName() {
+        WebElement sneakersName = driver.findElement(By.xpath("//h1[@class = 'product_title entry-title' and text() ='New Balance 574 Dark']"));
+        String txt = sneakersName.getText();
+        Assert.assertTrue(sneakersName.getText().contains("New Balance 574 Dark"));
+    }
+
+    @AfterClass
+    public void shutdown() {
+        driver.quit();
+    }
 }
