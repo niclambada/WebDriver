ea00ce1a0a9789f93b6df4e043468fffefe21fd3
Nikolay Tsvetkov <nic.tsvetcov@gmail.com>
2020-11-29 18:09:53 +0300

    rename classes

--- src/main/java/WebDriverSelenium.java
+++ src/main/java/WebDriverSelenium.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class SeleniumWebDriver {
+public class WebDriverSelenium {
 
     public static WebDriver driver;
     public static WebElement searchInput;
--- src/test/java/WebDriverSeleniumTest.java
+++ src/test/java/WebDriverSeleniumTest.java
@@ -5,7 +5,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class SeleniumWebDriverTest  extends SeleniumWebDriver {
+public class WebDriverSeleniumTest extends WebDriverSelenium {
 
     @BeforeTest
     public void setUp() {
